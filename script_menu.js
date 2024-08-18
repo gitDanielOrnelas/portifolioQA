feb4f37ec5efd410cf13b8ab13047ef4fc0b57af
Daniel Ornelas <106773127+gitDanielOrnelas@users.noreply.github.com>
2024-08-18 20:26:46 -0300

    up

---
+++ script_menu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu-container').innerHTML = data;
+    });
+});
